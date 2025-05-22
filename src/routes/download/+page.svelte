90c6b840b1665be9d77d2f6898ae8e12c269fad6
Taavi Rübenhagen <t.ruebenhagen@gmail.com>
2025-05-22 09:13:07 +0200

    Untitled

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -12,7 +12,7 @@
 >
     <p1>
         <a
-            download href="/presenter/Presentation Master 2.msi"
+            download href="/Presentation Master 2.msi"
             class=
             "transition-all duration-[200ms] relative
             border border-white rounded-full h-10 hover:opacity-80
